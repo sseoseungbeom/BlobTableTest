d447b7a5b3733b689bdc8b8e85bcc0ee75009090
sseoseungbeom <seungbami21@gmail.com>
2021-07-14 15:27:35 +0900

    11th

--- WriteTable.cs
+++ WriteTable.cs
@@ -14,23 +14,40 @@ namespace seungbeom.Function
     public static class WriteTable
     {
         [FunctionName("WriteTable")]
-        public static String Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] 
-            HttpRequest req, ILogger log, ExecutionContext context)
+       public static void Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        HttpRequest req, ILogger log, ExecutionContext context)
         {
             string connStrA = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
             string requestBody = new StreamReader(req.Body).ReadToEnd();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            string valueA = data.a;
+            string PartitionKeyA = data.PartitionKey;
+            string RowKeyA = data.RowKey;
+            string contentA = data.Content;
 
             CloudStorageAccount stoA = CloudStorageAccount.Parse(connStrA);
             CloudTableClient tbC = stoA.CreateCloudTableClient();
             CloudTable tableA = tbC.GetTableReference("tableA");
 
-
-            return "";
-
-        
+            writeToTable(tableA, contentA, PartitionKeyA, RowKeyA);
         }
+
+
+       static void writeToTable(CloudTable tableA, string contentA, string PartitionKeyA, string RowKeyA)
+        {
+            MemoData memoA = new MemoData();
+            memoA.PartitionKey = PartitionKeyA;
+            memoA.RowKey = RowKeyA;
+            memoA.content = contentA;
+
+            TableOperation operA = TableOperation.InsertOrReplace(memoA);
+            tableA.Execute(operA);
+        }
+
+
+      private class MemoData : TableEntity
+        {
+            public string content { get; set; }
+        }
+
     }
 }
