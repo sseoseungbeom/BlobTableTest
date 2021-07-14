dcd7088565e3b149d96b788b652cb9155b6bdbb5
sseoseungbeom <seungbami21@gmail.com>
2021-07-14 14:27:11 +0900

    2nd

--- GetJSONData.cs
+++ GetJSONData.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Storage.Blobs;
 
 namespace seungbeom.Function
 {
@@ -19,11 +20,27 @@ namespace seungbeom.Function
         {
             string connStrA = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
             string requestBody = new StreamReader(req.Body).ReadToEnd();
-            
             dynamic data = JsonConvert.DeserializeObject(requestBody);
              string valueA =  data.a;
                 //a가 valueA에 받아져서 requestBody에 넘김
-            return  valueA;
+
+               BlobServiceClient clientA = new BlobServiceClient(connStrA);
+               BlobServiceClient containerA = clientA.getBlobContainerClient("sbcon");
+               BlobClient blobA = containerA.GetBlobClient(valuesA + ".json"); //안쪽에 파일명
+               //BlobClient = 특정파일을 의미
+
+               string responseA = "No Data";
+
+               if(blobA.Exists()) //blobA가존재하면 실행
+               {
+                   using (MemoryStream msA = new MemoryStream())
+                   {
+                       blobA.DownloadTo(msA);
+                        responseA = System.Text.Encoding.UTF8.GetString(msA.ToArray());
+                   }
+               }
+
+            return  responseA;
         }
     }
 }
